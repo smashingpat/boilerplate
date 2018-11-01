6eba0fe9f73d855ee04b0d54b5287e6a772d0d2e
Patrick Gerritsen <p.gerritsen@wearetriple.com>
2018-11-01 10:59:50 +0100

    added update method

--- source/view/App.tsx
+++ source/view/App.tsx
@@ -1,23 +1,8 @@
 import * as React from "react";
 import { createPortal } from "react-dom";
 import { hot } from "react-hot-loader";
+import debounce from "lodash/debounce";
 import styles from "./App.scss";
-
-const columns = [...Array(5)].map(() => ({
-    id: Math.floor(Math.random() * 1e8),
-    rows: [...Array(10)].map(() => ({
-        id: Math.floor(Math.random() * 1e8),
-    })),
-}));
-
-interface HeaderState {
-    top: number;
-    left: number;
-    height: number;
-    width: number;
-}
-
-interface HeaderProps {}
 
 function getElementOffset(el: HTMLElement) {
     let top = 0;
@@ -29,6 +14,8 @@ function getElementOffset(el: HTMLElement) {
     do {
       top += element.offsetTop || 0;
       left += element.offsetLeft || 0;
+      // @ts-ignore ignore as there is no easy way to guard the Type
+      // without the use of instanceOf HTMLElement
       element = element.offsetParent;
     } while (element);
 
@@ -38,11 +25,24 @@ function getElementOffset(el: HTMLElement) {
     };
 }
 
+interface HeaderState {
+    top: number;
+    left: number;
+    height: number;
+    width: number;
+}
+
+interface HeaderProps {
+    text: string;
+}
+
 class Header extends React.Component<HeaderProps, HeaderState> {
     mountElement: HTMLDivElement;
 
     headerRef = React.createRef<HTMLDivElement>();
     pushRef = React.createRef<HTMLDivElement>();
+
+    handleWindowResize = debounce(() => this.setState({}), 500);
 
     constructor(props: HeaderProps) {
         super(props);
@@ -53,10 +53,12 @@ class Header extends React.Component<HeaderProps, HeaderState> {
 
     componentDidMount() {
         this.setState({});
+        window.addEventListener("resize", this.handleWindowResize);
     }
 
     componentWillUnmount() {
         document.body.removeChild(this.mountElement);
+        window.removeEventListener("resize", this.handleWindowResize);
     }
 
     componentDidUpdate() {
@@ -73,6 +75,7 @@ class Header extends React.Component<HeaderProps, HeaderState> {
             (this.state.height !== height) ||
             (this.state.width !== width)
         ) {
+            console.log("updated location");
             this.setState({ height, width, top, left });
         }
     }
@@ -80,7 +83,7 @@ class Header extends React.Component<HeaderProps, HeaderState> {
         return (
             <>
                 {createPortal((
-                    <div style={{ top: this.state.top, left: this.state.left, width: this.state.width }} ref={this.headerRef} className={styles.header}>header</div>
+                    <div style={{ top: this.state.top, left: this.state.left, width: this.state.width }} ref={this.headerRef} className={styles.header}>{this.props.text}</div>
                 ), this.mountElement)}
                 <div ref={this.pushRef} style={{ height: this.state.height }} className={styles.headerPush} />
             </>
@@ -88,31 +91,47 @@ class Header extends React.Component<HeaderProps, HeaderState> {
     }
 }
 
-class App extends React.Component {
+interface AppState {
+    columns: Array<{
+        id: number,
+        rows: Array<{
+            id: number,
+        }>,
+    }>;
+}
+
+class App extends React.Component<{}, AppState> {
+    state = {
+        columns: [...Array(5)].map(() => ({
+            id: Math.floor(Math.random() * 1e8),
+            rows: [...Array(3)].map(() => ({
+                id: Math.floor(Math.random() * 1e8),
+            })),
+        })),
+    };
+    addRow = () => this.setState((prevState) => ({
+        columns: prevState.columns.map((column) => ({ ...column, rows: column.rows.concat({ id: Math.floor(Math.random() * 1e8) })})),
+    }))
     render() {
+        const { columns } = this.state;
         return (
-            <section className={styles.container} onClick={() => this.forceUpdate()}>
+            <section className={styles.container}>
                 <div className={styles.containerInner}>
-                    <Header />
-                    <div className={styles.columnContainer}>
-                        {columns.map((column) => (
-                            <div key={column.id} className={styles.column}>
-                                {column.rows.map((row) => (
-                                    <div key={row.id} className={styles.row} />
+                    {[...Array(5)].map((_, index) => (
+                        <React.Fragment key={index}>
+                            <Header text={`Header ${index + 1}`} />
+                            <div className={styles.columnContainer}>
+                                {columns.map((column) => (
+                                    <div key={column.id} className={styles.column}>
+                                        {column.rows.map((row) => (
+                                            <div key={row.id} className={styles.row} />
+                                        ))}
+                                        <button className={[ styles.row, styles.isButton ].join(" ")} onClick={this.addRow} />
+                                    </div>
                                 ))}
                             </div>
-                        ))}
-                    </div>
-                    <Header />
-                    <div className={styles.columnContainer}>
-                        {columns.map((column) => (
-                            <div key={column.id} className={styles.column}>
-                                {column.rows.map((row) => (
-                                    <div key={row.id} className={styles.row} />
-                                ))}
-                            </div>
-                        ))}
-                    </div>
+                        </React.Fragment>
+                    ))}
                 </div>
             </section>
         );
