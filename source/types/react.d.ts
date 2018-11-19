9ed86e5a27abc1a1649ea7a1d240052a2d83cbdb
Patrick Gerritsen <p.gerritsen@wearetriple.com>
2018-11-19 15:25:06 +0100

    updated React types

--- source/types/react.d.ts
+++ source/types/react.d.ts
@@ -1,10 +1,4 @@
 import * as React from 'react';
 
-// Extra types from React 16.5.4 -> 16.6.0
-declare module 'react' {
-    export const Suspense: React.ComponentType<{ fallback: React.ReactNode }>
-    export function memo<C extends React.ComponentType<any>>(Component: C): C
-    export function lazy<C extends React.ComponentType<any>>(
-        promise: () => Promise<{ default: C }>
-    ): C
-}
+// Extra types from React 16.6.3
+declare module 'react';
