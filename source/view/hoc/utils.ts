085023120e21447f60f01fcc0658b9a032b486cf
Patrick Gerritsen <p.gerritsen@wearetriple.com>
2019-01-09 09:39:25 +0100

    removed predefined HOCs

--- source/view/hoc/utils.ts
+++
@@ -1,9 +0,0 @@
-import * as React from "react";
-
-export function getDisplayName(Component: React.ComponentType) {
-    return Component.displayName || Component.name || "undefined";
-}
-
-export function createDisplayName(displayName: string, Component: React.ComponentType) {
-    return `${displayName}("${getDisplayName(Component)}")`;
-}
